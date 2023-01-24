f903bc5d504c5448b7ed4380dd376126052a3b94
Vladimir Klevtsov <vladimirklevtsovmain@gmail.com>
2023-01-24 14:46:42 +0300

    Add animations

---
+++ Assets/Sourse/Player/Animations/AnimationsScripts/PlayerAnimationsController.cs
@@ -0,0 +1,12 @@
+public class PlayerAnimationsController 
+{
+    public static class Params
+    {
+        public static string Run = nameof(Run);
+    }
+
+    public static class States
+    {
+        public static string Running = nameof(Running);
+    }
+}
