67bd23baa832ca440cdc66e7bd42b2a4fcccc5c3
Vladimir Klevtsov <vladimirklevtsovmain@gmail.com>
2023-01-25 17:35:16 +0300

    Changes on test scene

--- Assets/Sourse/JumpPanel/JumpPanel.cs
+++ Assets/Sourse/JumpPanel/JumpPanel.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using UnityEngine;
 
+[RequireComponent(typeof(Bezier))]
 public class JumpPanel : MonoBehaviour
 {
-    [SerializeField] private Bezier _bezier;
     [SerializeField] private Transform _point1;
     [SerializeField] private Transform _point2;
     [SerializeField] private Transform _point3;
     [SerializeField] private Transform _point4;
+    [SerializeField] private Bezier _bezier;
+    [SerializeField] private float _jumpSpeed = 0.01f;
 
     private float _time;
+
+    private void Start()
+    {
+        _bezier = GetComponent<Bezier>();
+    }
+
+    private IEnumerator JumpCoroutine(Transform player)
+    {
+        while (_time < 1)
+        {
+            _time = Mathf.MoveTowards(_time, 1, _jumpSpeed);
+            player.position = _bezier.GetPoint(_point1.position, _point2.position, _point3.position, _point4.position, _time);
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    public void Jump(Transform player)
+    {
+        StartCoroutine(JumpCoroutine(player));
+    }
 
     private void OnDrawGizmos()
     {
@@ -23,22 +46,5 @@ public class JumpPanel : MonoBehaviour
             Gizmos.DrawLine(preveousePoint, point);
             preveousePoint = point;
         }
-    }
-
-    private IEnumerator JumpCoroutine(Transform player)
-    {
-        while (_time < 1)
-        {
-            _time = Mathf.MoveTowards(_time, 1, 0.01f);
-            Debug.Log(_time);
-            player.position = _bezier.GetPoint(_point1.position, _point2.position, _point3.position, _point4.position, _time);
-
-            yield return new WaitForFixedUpdate();
-        }
-    }
-
-    public void Jump(Transform player)
-    {
-        StartCoroutine(JumpCoroutine(player));
     }
 }
