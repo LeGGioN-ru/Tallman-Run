91a4c97420d64d073be4e9360eefd76f2a0704be
LeGGioN-ru <nikita90017@gmail.com>
2023-01-31 15:20:28 +0300

    Update upgrades prefab, also complete speed upgrade

--- Assets/Sourse/Upgrade/SpeedUpgrade.cs
+++ Assets/Sourse/Upgrade/SpeedUpgrade.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class SpeedUpgrade : Upgrader
 {
-    //[SerializeField] private 
+    [SerializeField] private PlayerMover _playerMover;
     [SerializeField] private float _speedUpgrade;
 
     protected override void Upgrade()
     {
-
+        _playerMover.IncreaseMoveSpeed(_speedUpgrade);
     }
 }
