23f5945f82815ad9b13b9349133480538c74a3d0
Vladimir Klevtsov <vladimirklevtsovmain@gmail.com>
2023-02-02 13:38:00 +0300

    Add icon for muted sound

--- Assets/Sourse/Sound/SoundButton.cs
+++ Assets/Sourse/Sound/SoundButton.cs
@@ -1,9 +1,27 @@
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class SoundButton : MonoBehaviour
 {
+    [SerializeField] private Sprite _volumeOn;
+    [SerializeField] private Sprite _volumeOff;
+
+    private Image _image;
+
+    private void Start()
+    {
+        _image = GetComponent<Image>();
+    }
+
     public void OnSoundButton()
     {
         SoundMuter.Instance.OnMuteButton();
+
+        if (AudioListener.volume == 0)
+            _image.sprite = _volumeOff;
+       else
+            _image.sprite = _volumeOn;
+
     }
 }
