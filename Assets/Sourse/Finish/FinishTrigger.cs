ef6f926e3e6f57b2e15b9640a9f6357a45b71ab5
Vladimir Klevtsov <vladimirklevtsovmain@gmail.com>
2023-02-02 14:43:45 +0300

    Work with finish

--- Assets/Sourse/Finish/FinishTrigger.cs
+++ Assets/Sourse/Finish/FinishTrigger.cs
@@ -4,6 +4,7 @@ public class FinishTrigger : MonoBehaviour
 {
     [SerializeField] private ADShower _ad;
     [SerializeField] private GameObject _videoButton;
+    [SerializeField] private ParticleSystem _confetti;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -11,6 +12,7 @@ public class FinishTrigger : MonoBehaviour
         {
             playerBehaviour.StartFinish();
 
+            _confetti.Play();
             _videoButton.SetActive(true);
             _ad.ShowAD();
         }
--- Assets/Sourse/UI/StartGameButton.cs
+++ Assets/Sourse/UI/StartGameButton.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartGameButton : MonoBehaviour
 {
     [SerializeField] private PlayerBehaviour _playerBehaviour;
+    [SerializeField] private Button _shopButton;
+    [SerializeField] private GameObject _upgrades;
 
     public void OnStartGame()
     {
-        gameObject.SetActive(false);
-
+        _shopButton.gameObject.SetActive(false);
+        _upgrades.SetActive(false);
         _playerBehaviour.Play();
+
+        gameObject.SetActive(false);
     }    
 }
