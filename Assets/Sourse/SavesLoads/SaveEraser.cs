a6c72d0bc6a7ed9382f6feb8e2809fc7dabf10b3
LeGGioN-ru <nikita90017@gmail.com>
2023-02-08 02:26:30 +0300

    Add save eraser

--- Assets/Sourse/SavesLoads/SaveEraser.cs
+++ Assets/Sourse/SavesLoads/SaveEraser.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class SaveEraser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        PlayerPrefs.DeleteAll();
+        PlayerPrefs.Save();
     }
 }
