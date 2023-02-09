c1a62f33fddad03ee1f5234690b4c28e304e35cb
LeGGioN-ru <nikita90017@gmail.com>
2023-02-09 20:18:32 +0300

    fix ui on all scenes, add load screen

--- Assets/Sourse/SavesLoads/StartSceneDefinder.cs
+++ Assets/Sourse/SavesLoads/StartSceneDefinder.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 using Agava.YandexGames;
 using Lean.Localization;
 
+[RequireComponent(typeof(LoadScreenStarter))]
 public class StartSceneDefinder : MonoBehaviour
 {
     private IEnumerator Start()
@@ -19,9 +20,12 @@ public class StartSceneDefinder : MonoBehaviour
 
         Save save = GameSaver.GetCurrentSave();
 
+        GetComponent<LoadScreenStarter>().Execute();
+
         if (save == null)
             Level_1.Load();
         else
             SceneManager.LoadScene(save.CurrentScene);
+
     }
 }
